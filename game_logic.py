6aa0b99b3b1a0b402e3483578e9599bffcb6e4cf
Yanu <59870900+khusanova@users.noreply.github.com>
2026-03-19 21:48:12 +0100

    Move game logic into a separate module

---
+++ game_logic.py
@@ -0,0 +1,49 @@
+"""Game logic module.
+
+Import play_game to run the game.
+"""
+import random
+
+from ascii_art import STAGES
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    print("Word: ", end = "")
+    for letter in secret_word:
+        if letter in guessed_letters:
+            print(letter, end = "")
+        else:
+            print("_", end = "")
+    print("\n")
+
+def play_game():
+    secret_word = get_random_word()
+    all_letters = set(list(secret_word))
+    print("Welcome to Snowman Meltdown!")
+    print(
+        "Secret word selected: " + secret_word)  # for testing, later remove this line
+
+    mistakes = 0
+    guessed_letters = []
+    while True:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        # For now, simply prompt the user once:
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        if guess in secret_word:
+            guessed_letters.append(guess)
+            if set(guessed_letters) == all_letters:
+                break
+        else:
+            mistakes += 1
+            if mistakes > 3:
+                break
--- main.py
+++ main.py
@@ -1,46 +1,4 @@
-import random
-
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
-
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
-
-
-def display_game_state(mistakes, secret_word, guessed_letters):
-    print(STAGES[mistakes])
-    print("Word: ", end = "")
-    for letter in secret_word:
-        if letter in guessed_letters:
-            print(letter, end = "")
-        else:
-            print("_", end = "")
-    print("\n")
-
-def play_game():
-    secret_word = get_random_word()
-    all_letters = set(list(secret_word))
-    print("Welcome to Snowman Meltdown!")
-    print(
-        "Secret word selected: " + secret_word)  # for testing, later remove this line
-
-    mistakes = 0
-    guessed_letters = []
-    while True:
-        display_game_state(mistakes, secret_word, guessed_letters)
-        # For now, simply prompt the user once:
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
-        if guess in secret_word:
-            guessed_letters.append(guess)
-            if set(guessed_letters) == all_letters:
-                break
-        else:
-            mistakes += 1
-            if mistakes > 3:
-                break
+from game_logic import play_game
 
 
 if __name__ == "__main__":
